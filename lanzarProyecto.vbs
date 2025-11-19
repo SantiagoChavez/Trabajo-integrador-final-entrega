83e08d7212320f92bf958f82a30e69bb52c770fe
SantiagoChavez <chavezsantiago480@gmail.com>
2025-11-19 20:13:41 -0300

    Lanzadores

---
+++ lanzarProyecto.vbs
@@ -0,0 +1,3 @@
+Set shell = CreateObject("WScript.Shell")
+shell.Run "cmd /c C:\TalentoTech\Trabajo-entregaFinal\start_proyecto.bat", 0, False
+MsgBox "Iniciando aplicacion... Por favor espere unos segundos.", vbInformation, "UTN FullTech"
--- start_proyecto.bat
+++ start_proyecto.bat
@@ -2,23 +2,37 @@
 title Arranque del Proyecto Fullstack
 
 echo -------------------------------
+echo Iniciando ejecutable de MongoDB...
+echo -------------------------------
+
+:: ¡¡IMPORTANTE!!
+:: Ajusta estas dos rutas (paths) a las de tu computadora.
+set MONGO_PATH="C:\Program Files\MongoDB\Server\8.0\bin\mongod.exe"
+set DB_PATH="C:\data\db"
+
+:: Asegúrate que la carpeta en DB_PATH (ej: C:\data\db) exista
+start "MongoDB Server" %MONGO_PATH% --dbpath %DB_PATH%
+timeout /t 2 /nobreak > nul
+
+echo -------------------------------
 echo Iniciando Backend (Spring Boot)...
 echo -------------------------------
-start cmd /k "cd /d C:\TalentoTech\Trabajo-entregaFinal\backend && mvn spring-boot:run"
+:: MODIFICADO: Se agrega > nul 2>&1 para descartar la salida
+start /B "Backend" cmd /c "cd /d C:\TalentoTech\Trabajo-entregaFinal\backend && mvn spring-boot:run > nul 2>&1"
 
-timeout /t 10 /nobreak > nul
+timeout /t 5 /nobreak > nul
 
 echo -------------------------------
 echo Iniciando Frontend (React/Vite)...
 echo -------------------------------
-start cmd /k "cd /d C:\TalentoTech\Trabajo-entregaFinal\frontend && npm run dev"
+:: MODIFICADO: Se agrega > nul 2>&1 para descartar la salida
+start /B "Frontend" cmd /c "cd /d C:\TalentoTech\Trabajo-entregaFinal\frontend && npm run dev > nul 2>&1"
 
-timeout /t 10 /nobreak > nul
+timeout /t 3 /nobreak > nul
 
 echo -------------------------------
 echo Abriendo navegador en el frontend...
 echo -------------------------------
 start http://localhost:5176
 
-echo Listo! Backend y Frontend arrancados.
-pause
+echo Listo! MongoDB, Backend y Frontend arrancados.
